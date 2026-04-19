ae3864c4152c1ab973447c1d057e0dddf81a9373
ppebb <ppeb@ppeb.me>
2026-04-18 23:18:00 -0500

    Mem writing

--- src/mem.h
+++ src/mem.h
@@ -8,4 +8,4 @@ void mem_set_cart_bank0(uint8_t *cb0);
 void mem_set_cart_bank1(uint8_t *cb1);
 
 uint8_t mem_read_byte(uint16_t addr);
-void mem_write_byte(uint16_t addr);
+void mem_write_byte(uint16_t addr, uint8_t data);
